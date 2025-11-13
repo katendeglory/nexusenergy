6d1898fa7ac0a279641617a349aba7f0b7b2df3b
Glory Katende <katendeglory@gmail.com>
2025-11-13 22:36:18 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
   import Hero from "../components/index/Hero.svelte";
   import About1 from "./../components/index/About1.svelte";
   import About2 from "./../components/index/About2.svelte";
+  import About3 from "./../components/index/About3.svelte";
+  import About4 from "./../components/index/About4.svelte";
   import SectionList1 from "./../components/index/SectionList1.svelte";
   import Gallery from "./../components/index/Gallery.svelte";
   import Contact1 from "../components/index/Contact1.svelte";
@@ -19,10 +21,16 @@
 </svelte:head>
 
 <Hero />
+
 <!--  -->
+
 <About1 />
-<About2 />
+
+<!-- <About2 /> -->
 <!--  -->
+
+<About3 />
+<About4 />
 
 <!--  -->
 <!-- <Services /> -->
